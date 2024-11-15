bcdae94f6c1c73cf616ce8d10e5bc6a397e70bf7
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-11-15 08:58:46 +0300

    Добавлен контроллер для авторизации.

--- LabAllianceTestAPIDbContext.cs
+++ LabAllianceTestAPIDbContext.cs
@@ -1,15 +1,25 @@
 ﻿using LabAllianceTest.API.Entities;
 using Microsoft.EntityFrameworkCore;
+using OpenIddict.EntityFrameworkCore.Models;
 
 namespace LabAllianceTest.API
 {
     public class LabAllianceTestAPIDbContext : DbContext
     {
+        public LabAllianceTestAPIDbContext(DbContextOptions<LabAllianceTestAPIDbContext> options)
+            : base(options) { }
+
+        public DbSet<OpenIddictEntityFrameworkCoreApplication> OpenIddictApplications { get; set; }
+        public DbSet<OpenIddictEntityFrameworkCoreAuthorization> OpenIddictAuthorizations { get; set; }
+        public DbSet<OpenIddictEntityFrameworkCoreScope> OpenIddictScopes { get; set; }
+        public DbSet<OpenIddictEntityFrameworkCoreToken> OpenIddictTokens { get; set; }
         public DbSet<UserEntity> Users { get; set; }
 
-        public LabAllianceTestAPIDbContext(DbContextOptions<LabAllianceTestAPIDbContext> options) : base(options)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            Database.EnsureCreated();
+            base.OnModelCreating(builder);
+
+            builder.UseOpenIddict();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ using LabAllianceTest.API.Helpers;
 using LabAllianceTest.API.Middleware;
 using LabAllianceTest.API.Repositories;
 using LabAllianceTest.API.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,32 +25,71 @@ builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(nameof(J
 var jwtOptions = builder.Configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
 
 // Add JWT bearer authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-    {
-        options.TokenValidationParameters = new()
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions?.SecretKey ?? "secretkeysecretkeysecretkeysecretkeysecretkeysecretkeysecretkey"))
-        };
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+//    {
+//        options.TokenValidationParameters = new()
+//        {
+//            ValidateIssuer = false,
+//            ValidateAudience = false,
+//            ValidateLifetime = true,
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions?.SecretKey ?? "secretkeysecretkeysecretkeysecretkeysecretkeysecretkeysecretkey"))
+//        };
 
-        //options.Events = new JwtBearerEvents
-        //{
-        //    OnMessageReceived = context =>
-        //    {
-        //        context.Token = context.Request.Cookies["test-cookies"];
-        //        return Task.CompletedTask;
-        //    }
-        //};
-    });
+//        //options.Events = new JwtBearerEvents
+//        //{
+//        //    OnMessageReceived = context =>
+//        //    {
+//        //        context.Token = context.Request.Cookies["test-cookies"];
+//        //        return Task.CompletedTask;
+//        //    }
+//        //};
+//    });
 
 builder.Services.AddDbContext<LabAllianceTestAPIDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseOpenIddict();
 });
+
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+                .UseDbContext<LabAllianceTestAPIDbContext>();
+    })
+    .AddServer(options =>
+    {
+        options.SetAuthorizationEndpointUris("connect/authorize")
+                .SetLogoutEndpointUris("connect/logout")
+                .SetTokenEndpointUris("connect/token")
+                .SetUserinfoEndpointUris("connect/userinfo");
+
+        options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
+
+        options.AllowAuthorizationCodeFlow();
+
+        options.AddEncryptionKey(new SymmetricSecurityKey(
+            Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
+
+        options.AddDevelopmentEncryptionCertificate()
+                .AddDevelopmentSigningCertificate();
+
+        options.UseAspNetCore()
+                .EnableAuthorizationEndpointPassthrough()
+                .EnableLogoutEndpointPassthrough()
+                .EnableTokenEndpointPassthrough()
+                .EnableUserinfoEndpointPassthrough();
+    });
+
+builder.Services.AddTransient<AuthorizationService>();
+
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(c =>
+//    {
+//        c.LoginPath = "/Authenticate";
+//    });
 
 builder.Services.AddScoped<IJwtProvider, JwtProvider>();
 
@@ -56,8 +97,21 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<ClientsSeeder>();
 
 builder.Services.AddAuthorization();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:7243")
+            .AllowAnyHeader();
+
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -78,5 +132,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<ClientsSeeder>();
+    seeder.AddOidcDebuggerClient().GetAwaiter().GetResult();
+    seeder.AddWebClient().GetAwaiter().GetResult();
+    seeder.AddScopes().GetAwaiter().GetResult();
+}
 
 app.Run();
